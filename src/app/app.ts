906064c8b4a196f80c16f0ea18c90d92d511dfa0
MohammadKaleaji <mohammadkaleaji@gmail.com>
2025-08-25 17:55:34 +0300

    display map 1st step

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+import "@arcgis/map-components/components/arcgis-map";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
+  standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  templateUrl: "./app.html",
+  styleUrl: "./app.scss",
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // Set the schema here
 })
 export class App {
-  protected readonly title = signal('arcgis-hello');
+  arcgisViewReadyChange(event: CustomEvent) {
+    // The view is ready, add additional functionality below
+  }
 }
